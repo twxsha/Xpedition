a0a5d906f01b98fab9ba74799842d9700c7d19b5
kratik-agrawal <75349834+kratik-agrawal@users.noreply.github.com>
2024-02-23 22:56:57 -0500

    adding homepage code to frontend

--- ui/src/Home.js
+++ ui/src/Home.js
@@ -30,17 +30,87 @@ const Home = () => {
     const handlePacklistClick = (e) => {
         setPacklist(e.target.value);
     };
-    return (
-        <div className="home">
-            <header className="header">
-                <div className='form'>
-                    <div className='logo'>
-                        <img src={logo} alt="logo" />
-                    </div>
+
+    const handleRunClick = () => {
+        // Implement the logic for when the run button is clicked
+        // You might want to navigate to another page with the data or send it to an API
+        console.log('Running with the following details:', { input, flights, stay, weather, activities, packlist });
+        // navigate('/some-result-page');
+    };
+
+return (
+    <div className="home">
+        <header className="header">
+            <img src={logo} alt="logo" className="logo" />
+            <div className="description">
+                <div className="input-group">
+                    <label className="input-label">Describe Your Vacation</label>
+                    <input
+                        type="text"
+                        value={input}
+                        onChange={handleInputChange}
+                        placeholder="Tell us details that will help us plan! For example, city you are starting from, place you want to go, dates you want to visit, number of people, etc..."
+                        className="input-description"
+                    />
                 </div>
-            </header>
-        </div>
-    );
+
+                <button onClick={handleRunClick} className="run-button">Run</button>
+            </div>
+
+        <main className="main-content">
+            <div className="row">
+                <div className="input-group">
+                    <label className="input-label">Flights</label>
+                    <textarea
+                        className="input-large"
+                        value={flights}
+                        onChange={handleflightsChange}
+                        placeholder="..."
+                    ></textarea>
+                </div>
+                <div className="input-group">
+                    <label className="input-label">Stays</label>
+                    <textarea
+                        className="input-large"
+                        value={stay}
+                        onChange={handleStayChange}
+                        placeholder="..."
+                    ></textarea>
+                </div>
+            </div>
+            <div className="row">
+                <div className="input-group">
+                    <label className="input-label">Weather</label>
+                    <textarea
+                        className="input-small"
+                        value={weather}
+                        onChange={handleWeatherChange}
+                        placeholder="..."
+                    ></textarea>
+                </div>
+                <div className="input-group">
+                    <label className="input-label">Activities</label>
+                    <textarea
+                        className="input-small"
+                        value={activities}
+                        onChange={handleActivitiesClick}
+                        placeholder="..."
+                    ></textarea>
+                </div>
+                <div className="input-group">
+                    <label className="input-label">Packing List</label>
+                    <textarea
+                        className="input-small"
+                        value={packlist}
+                        onChange={handlePacklistClick}
+                        placeholder="..."
+                    ></textarea>
+                </div>
+            </div>
+        </main>
+        </header>
+    </div>
+);
 };
 
 export default Home;
