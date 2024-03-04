4d841a588416898411d92bd513a40bf2f3273996
Nitin Subramanian <nitins@ucla.edu>
2024-03-03 21:32:11 -0800

    More fixes

---
+++ src/app/landing/page.js
@@ -0,0 +1,30 @@
+'use client';
+
+import React from 'react';
+import './landing.css';
+import { useRouter } from 'next/navigation';
+import logo from '@/public/logo.png';
+import Link from 'next/link';
+
+const Page = () => {
+    const navigate = useRouter();
+    const handleSignupClick = () => {
+        //navigate('/signup');
+    };
+    const handleLogInClick = () => {
+        //navigate('/login');
+    };
+    return (
+        <div className="landing">
+            <header className="header">
+                <img src={logo.src} className="flex items-center justify-center mb-40" alt="logo" />
+                <div className='buttons'>
+                <Link href='signup'><button className='signupbutton' onClick={handleSignupClick}> <p>Sign Up</p> </button></Link>
+                <Link href='login'><button className='loginbutton' onClick={handleLogInClick}> <p>Log In</p> </button></Link>
+                </div>
+            </header>
+        </div>
+    );
+};
+
+export default Page;
--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,13 @@
 'use client';
 
 import React from 'react';
-import './Landing.css';
-import { useRouter } from 'next/navigation';
-import logo from '@/public/logo.png';
-import Link from 'next/link';
+import Landing from './landing/page';
 
 const Page = () => {
-    const navigate = useRouter();
-    const handleSignupClick = () => {
-        //navigate('/signup');
-    };
-    const handleLogInClick = () => {
-        //navigate('/login');
-    };
+   
     return (
-        <div className="landing">
-            <header className="header">
-                <img src={logo.src} className="flex items-center justify-center mb-40" alt="logo" />
-                <div className='buttons'>
-                <Link href='signup'><button className='signupbutton' onClick={handleSignupClick}> <p>Sign Up</p> </button></Link>
-                <Link href='login'><button className='loginbutton' onClick={handleLogInClick}> <p>Log In</p> </button></Link>
-                </div>
-            </header>
+        <div>
+          <Landing />
         </div>
     );
 };
