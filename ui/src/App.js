630fd36c4f25ced476a4aafff5d9fe42ff4b7dce
Arpit Jalan <jalanarpit12@gmail.com>
2024-03-05 20:04:35 -0800

    Add shoutout to serp api

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,13 +1,16 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Landing from './Landing';
 import SignUp from './SignUp';
 import LogIn from './LogIn';
 import Home from './Home';
-
-
+import './App.css';
 function App() {
   return (
-    <Router>
+
+  <Router>
+    <Link to="https://serpapi.com/">
+      <img id = "serp_callout" src="/serp_api.png" alt = "Powered by SerpAPI" href = "https://serpapi.com/"/>
+    </Link>
     <Routes>
       <Route exact path="" element={<Landing />} />
       <Route path="/X" element={<Home />} />
