fb09d4748400379d3f570b07329e09f6e2396a4c
Sankirth Vutukuri <svutukuri67@gmail.com>
2024-03-08 14:36:52 -0800

    Packing list

---
+++ endpoints/packing/getPackingList.js
@@ -0,0 +1,47 @@
+const { OpenAI } = require("openai");
+require('dotenv').config()
+
+const openai = new OpenAI();
+
+async function getPackingList() {
+    initial_prompt = "Help me plan a trip for Hawaii"
+    const gptResponse = await openai.chat.completions.create({
+        model: "gpt-4-turbo-preview",
+        messages: [
+            {
+                role: "system",
+                content: "Given a prompt, create a packing list for this location considering climate. Please return items with emojis if applicable and limit the output to 25 items"
+            },
+            {
+                role: "user", 
+                content: "Create a packing list for this prompt " + initial_prompt
+            }
+        ],
+        functions: [
+            {
+                name: "createPackingList",
+                parameters: {
+                    type: "object",
+                    properties: {
+                        packing_list: {
+                            type: "array",
+                            items: { type: "string" },
+                            description: "An array of items (including emojis for these items) in a packing list"
+                        }
+                    },
+                    required: ["packing_list"]
+                }
+            }
+        ],
+        function_call: { name: "createPackingList" }
+    });
+
+    const functionCall = gptResponse.choices[0].message.function_call;
+    const packing_list = functionCall['arguments'];
+
+    console.log(packing_list);
+
+    return packing_list;
+}
+
+export default getPackingList;
